3cb44219c1ebfcad72946175a4e98e2492852d38
JG <git@lostuxos.de>
2020-11-29 17:58:34 +0100

    Step 1: Finished

--- src/main/java/de/bpghub/edu/kata/christmastree/ChristmasTreeWriter.java
+++ src/main/java/de/bpghub/edu/kata/christmastree/ChristmasTreeWriter.java
@@ -3,6 +3,6 @@ package de.bpghub.edu.kata.christmastree;
 public class ChristmasTreeWriter {
 
     public String writeTree(int height) {
-        throw new RuntimeException("Not Implementet");
+        return "";
     }
 }
