4fc2f7a2301f2ab55e02482fe0c79eba08e42ef7
Jens Golombek <jens.golombek@bertelsmann.de>
2020-12-04 15:48:05 +0100

    Refactor

--- src/main/java/de/bpghub/edu/kata/christmastree/ChristmasTreeWriter.java
+++ src/main/java/de/bpghub/edu/kata/christmastree/ChristmasTreeWriter.java
@@ -14,9 +14,26 @@ public class ChristmasTreeWriter {
                         ^
                        / \\
                       /   \\
+                     /     \\
                         #
                       """;
             default -> throw new RuntimeException("Method not implemented");
+
+            // anzahl_stufen = height -1
+            // 0 = 1 space
+            // 1 = 3 space
+            // 2 = 5 space
+            // 3 = 7 space
+            //zwischenraum
+            // 0(i) * 2 + 1 = 1 space
+            // 1(i) * 2 + 1 = 3 space
+            // 2(i) * 2 + 1 = 5 space
+            // 3(i) * 2 + 1 = 7 space
+
+            //prefix
+            // 2(anzahl_stufen) - 0(i) = 2 spaces
+            // 2(anzahl_stufen) - 1(i) = 1 spaces
+            // 2(anzahl_stufen) - 2(i) = 0 spaces
         };
     }
 }
